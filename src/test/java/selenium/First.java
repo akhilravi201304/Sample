04e88040899487926d1750c8812f105e63261bac
akhil <akhilravi201304@gmail.com>
2021-04-13 12:19:56 +0530

    first commit

---
+++ src/test/java/selenium/First.java
@@ -0,0 +1,25 @@
+package selenium;
+
+import org.testng.annotations.Test;
+
+public class First {
+	
+	@Test
+	public void test1() {
+		// TODO Auto-generated method stub
+		System.out.println("test1");
+	}
+	
+	@Test
+	public void test2() {
+		// TODO Auto-generated method stub
+		System.out.println("test2");
+	}
+	
+	@Test
+	public void test3() {
+		// TODO Auto-generated method stub
+		System.out.println("test3");
+	}
+
+}
